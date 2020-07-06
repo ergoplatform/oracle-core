3794bbe58aea971c52d9a012ac02cd6c8e55bcea
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-07-06 15:09:06 -0400

    Basic json template for tx requests added

--- src/templates.rs
+++ src/templates.rs
@@ -1,28 +1,27 @@
 
 
-
-
-static COMMIT_ORACLE_DATAPOINT_REQUEST : &'static str = r#"{
+/// Tx Request Template 
+static BASIC_TRANSACTION_SEND_REQUEST : &'static str = r#"{
   "requests": [
     {
-      "address": "{{address}}",
+      "address": "",
       "value": 5000000,
       "assets": [
         {
-          "tokenId": "{{token_id}}",
+          "tokenId": "",
           "amount": 1
         }
       ],
       "registers": {
-        "R4": "{{R4}}",
-        "R5": "{{R5}}",
-        "R6": "{{R6}}"
+        "R4": "",
+        "R5": "",
+        "R6": ""
       }
     }
   ],
   "fee": 1000000,
   "inputsRaw": [ 
-    "{{ergs_raw_input}}",
-    "{{datapoint_box_raw_input}}"
+  ]
+  "dataInputsRaw": [
   ]
 }"#;
