55ab66db87d80da98317251afe6264039d68b7c8
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-07-13 12:38:28 -0400

    Implemented pool parameter parsing & test

--- src/oracle_config.rs
+++ src/oracle_config.rs
@@ -1,5 +1,35 @@
 /// Basic functions for acquiring oracle config/node data
-use yaml_rust::YamlLoader;
+use yaml_rust::{Yaml, YamlLoader};
+
+pub struct PoolParameters {
+    pub live_epoch_length: u64,
+    pub epoch_preparation_length: u64,
+    pub buffer_length: u64,
+}
+
+impl PoolParameters {
+    pub fn new() -> PoolParameters {
+        let config = &YamlLoader::load_from_str(&get_config_yaml()).unwrap()[0];
+        PoolParameters::new_from_yaml_string(config)
+    }
+
+    pub fn new_from_yaml_string(config: &Yaml) -> PoolParameters {
+        let lel = config["live_epoch_length"]
+            .as_i64()
+            .expect("No live_epoch_length specified in config file.");
+        let epl = config["epoch_preparation_length"]
+            .as_i64()
+            .expect("No epoch_preparation_length specified in config file.");
+        let buf = config["buffer_length"]
+            .as_i64()
+            .expect("No buffer_length specified in config file.");
+        PoolParameters {
+            live_epoch_length: lel as u64,
+            epoch_preparation_length: epl as u64,
+            buffer_length: buf as u64,
+        }
+    }
+}
 
 /// Reads the `oracle_config.yaml` file
 pub fn get_config_yaml() -> String {
@@ -38,5 +68,19 @@ mod tests {
     #[test]
     fn valid_api_key_from_config() {
         assert_eq!(get_node_api_key(), "hello".to_string())
+    }
+
+    #[test]
+    fn pool_parameter_parsing_works() {
+        let yaml_string = "
+            live_epoch_length: 20
+            epoch_preparation_length: 10
+            buffer_length: 4
+            ";
+        let config = &YamlLoader::load_from_str(yaml_string).unwrap()[0];
+        let pool_params = PoolParameters::new_from_yaml_string(&config);
+        assert_eq!(pool_params.live_epoch_length, 20);
+        assert_eq!(pool_params.epoch_preparation_length, 10);
+        assert_eq!(pool_params.buffer_length, 4);
     }
 }
