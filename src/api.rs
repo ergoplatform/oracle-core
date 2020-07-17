dffd5f6c54600e6a6abbaefe2f1fd8c8957313a7
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-07-17 10:25:52 -0400

    Implemented basic endpoints

--- src/api.rs
+++ src/api.rs
@@ -1,7 +1,9 @@
-use crate::oracle_config::PoolParameters;
+use crate::node_interface::current_block_height;
+use crate::oracle_config::{get_api_port, PoolParameters};
 use crate::oracle_state::OraclePool;
 use crossbeam::channel;
 use sincere;
+use std::panic::catch_unwind;
 
 /// Starts the API server
 pub fn start_api() {
@@ -16,5 +18,22 @@ pub fn start_api() {
         context.response.from_text(response_text).unwrap();
     });
 
-    app.run("0.0.0.0:9031", 5).unwrap();
+    app.get("/info", move |context| {
+        let response_text = format!(
+            "Local Oracle Address: {}\n
+            ",
+            op.local_oracle_address
+        );
+        context.response.from_text(response_text).unwrap();
+    });
+
+    app.get("/blockheight", move |context| {
+        let current_height =
+            current_block_height().expect("Please ensure that the Ergo node is running.");
+        let response_text = format!("{}", current_height);
+        context.response.from_text(response_text).unwrap();
+    });
+
+    // Start the API server with the port designated in the config.
+    app.run(&("0.0.0.0:".to_string() + &get_api_port()), 1).ok();
 }
