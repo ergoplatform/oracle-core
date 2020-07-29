356155d93d392468233de03b9ecc2e8ac2b062ca
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-07-29 11:21:40 -0400

    Added new serialized function + cleanup

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,3 +1,4 @@
+use base16;
 /// This file holds various functions related to encoding/serialization of values that are relevant
 /// to the oracle core.
 use sigma_tree::ast::{CollPrim, Constant, ConstantColl, ConstantVal};
@@ -31,9 +32,19 @@ pub fn serialize_long(i: i64) -> String {
 /// Serialize a `String` value into a hex-encoded string to be used inside of a register for a box
 pub fn serialize_string(s: &String) -> String {
     let a = s.clone().into_bytes();
-    let b: Vec<i8> = a.iter().map(|c| c.clone() as i8).collect();
+    let b = convert_to_signed_bytes(&a);
     let constant: Constant = b.into();
     constant.base16_str()
+}
+
+/// Decodes a hex-encoded string into bytes and then serializes it into a properly formatted hex-encoded string to be used inside of a register for a box
+pub fn serialize_hex_encoded_string(s: &String) -> Result<String> {
+    if let Ok(b) = base16::decode(s) {
+        let constant: Constant = convert_to_signed_bytes(&b).into();
+        return Ok(constant.base16_str());
+    } else {
+        return Err(EncodingError::FailedToSerialize(s.clone()));
+    }
 }
 
 /// Deserialize a hex-encoded `i32` Long inside of a `Constant` acquired from a register of a box
@@ -83,6 +94,11 @@ pub fn deserialize_ergo_tree(c: &Constant) -> Result<String> {
 /// Convert Vec<i8> to Vec<u8>
 fn convert_to_unsigned_bytes(bytes: &Vec<i8>) -> Vec<u8> {
     bytes.iter().map(|x| x.clone() as u8).collect()
+}
+
+/// Convert Vec<u8> to Vec<i8>
+fn convert_to_signed_bytes(bytes: &Vec<u8>) -> Vec<i8> {
+    bytes.iter().map(|x| x.clone() as i8).collect()
 }
 
 /// Convert from Erg to nanoErg
