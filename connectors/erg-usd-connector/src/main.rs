e5cd2f61453fd4efdb55702e94bf6a29539b3627
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-08-04 13:37:08 -0400

    Improved connector printing further

--- connectors/erg-usd-connector/src/main.rs
+++ connectors/erg-usd-connector/src/main.rs
@@ -5,14 +5,16 @@ use connector_lib::{get_core_api_port, OracleCore};
 /// Note: The value that is posted on-chain is the number
 /// of nanoErgs per 1 USD, not the rate per nanoErg.
 use json;
+use std::thread;
+use std::time::Duration;
 
 static CONNECTOR_ASCII: &str = r#"
-  ______ _____   _____        _    _  _____ _____     _____                            _             
- |  ____|  __ \ / ____|      | |  | |/ ____|  __ \   / ____|                          | |            
- | |__  | |__) | |  __ ______| |  | | (___ | |  | | | |     ___  _ __  _ __   ___  ___| |_ ___  _ __ 
+  ______ _____   _____        _    _  _____ _____     _____                            _
+ |  ____|  __ \ / ____|      | |  | |/ ____|  __ \   / ____|                          | |
+ | |__  | |__) | |  __ ______| |  | | (___ | |  | | | |     ___  _ __  _ __   ___  ___| |_ ___  _ __
  |  __| |  _  /| | |_ |______| |  | |\___ \| |  | | | |    / _ \| '_ \| '_ \ / _ \/ __| __/ _ \| '__|
- | |____| | \ \| |__| |      | |__| |____) | |__| | | |___| (_) | | | | | | |  __/ (__| || (_) | |   
- |______|_|  \_\\_____|       \____/|_____/|_____/   \_____\___/|_| |_|_| |_|\___|\___|\__\___/|_|   
+ | |____| | \ \| |__| |      | |__| |____) | |__| | | |___| (_) | | | | | | |  __/ (__| || (_) | |
+ |______|_|  \_\\_____|       \____/|_____/|_____/   \_____\___/|_| |_|_| |_|\___|\___|\__\___/|_|
  ==================================================================================================
 "#;
 
@@ -24,8 +26,12 @@ fn main() {
     let core_port = get_core_api_port().expect("Failed to read local `oracle-config.yaml`.");
     let oc = OracleCore::new("0.0.0.0", &core_port);
 
-    // Print ASCII
-    println!("{}", CONNECTOR_ASCII);
+    // Main Loop
+    loop {
+        print_info(&oc).unwrap();
+
+        thread::sleep(Duration::new(30, 0))
+    }
 
     let price_res = get_nanoerg_usd_price();
     if let Ok(price) = price_res {
@@ -35,6 +41,24 @@ fn main() {
     } else {
         println!("{:?}", price_res);
     }
+}
+
+/// Prints Connector ASCII/info
+fn print_info(oc: &OracleCore) -> Result<bool> {
+    let pool_status = oc.pool_status()?;
+    let oracle_status = oc.oracle_status()?;
+    print!("\x1B[2J\x1B[1;1H");
+    println!("{}", CONNECTOR_ASCII);
+    println!("Current Blockheight: {}", oc.current_block_height()?);
+    println!(
+        "Current Oracle Pool Stage: {}",
+        pool_status.current_pool_stage
+    );
+    println!(
+        "Submit Datapoint In Latest Epoch: {}",
+        !oracle_status.waiting_for_datapoint_submit
+    );
+    Ok(true)
 }
 
 /// Acquires the nanoErg/USD price from CoinGecko
