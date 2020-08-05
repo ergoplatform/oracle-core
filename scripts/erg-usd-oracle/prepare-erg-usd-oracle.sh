efa927b87e54065813b9c765ad73678c642b656b
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-08-05 10:35:07 -0400

    Wrote erg-usd oracle prep script

---
+++ scripts/erg-usd-oracle/prepare-erg-usd-oracle.sh
@@ -0,0 +1,14 @@
+# This script builds the oracle core & the erg-usd connector
+# and then copies the executables + config file into a final
+# `oracle-core-deployed` folder.
+
+mkdir ../../oracle-core-deployed
+cp run-erg-usd-oracle.sh ../../oracle-core-deployed
+cd ../..
+cp oracle-config.yaml oracle-core-deployed
+cargo build --release
+cp target/release/oracle-core oracle-core-deployed
+cd connectors/erg-usd-connector
+cargo build --release
+cp target/release/erg-usd-connector ../../oracle-core-deployed
+cd ../../oracle-core-deployed
