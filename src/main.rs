527c122b79f141f6e47ad7a8612f3f7b5b7d7bd4
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-08-11 09:49:32 -0400

    Fixed >= mistake in logic

--- src/main.rs
+++ src/main.rs
@@ -88,7 +88,7 @@ fn main() {
             // Check epoch prep state
             let is_funded = prep_state.funds >= parameters.max_pool_payout();
             let epoch_prep_over =
-                height >= prep_state.next_epoch_ends - parameters.live_epoch_length;
+                height > prep_state.next_epoch_ends - parameters.live_epoch_length;
             let live_epoch_over = height >= prep_state.next_epoch_ends;
 
             // The Pool is underfunded
