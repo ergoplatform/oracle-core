c2ecc255e49e4bd3ddbb60be1e13fd6d4544b657
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-08-17 11:44:47 -0400

    Exposed Datapoint publicly

--- connectors/connector-lib/src/lib.rs
+++ connectors/connector-lib/src/lib.rs
@@ -4,5 +4,5 @@ extern crate json;
 pub mod connector;
 pub mod oracle_core;
 
-pub use connector::Connector;
+pub use connector::{Connector, Datapoint};
 pub use oracle_core::{get_core_api_port, OracleCore};
