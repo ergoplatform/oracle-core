391000e854ecb8a1f692d67efaebdb6f64ae32eb
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-08-20 12:44:25 -0400

    Cleaned up erg-usd connector code

--- connectors/erg-usd-connector/src/main.rs
+++ connectors/erg-usd-connector/src/main.rs
@@ -7,22 +7,15 @@
 use anyhow::{anyhow, Result};
 use frontend_connector_lib::FrontendConnector;
 
-static CONNECTOR_ASCII: &str = r#"
- ______ _____   _____        _    _  _____ _____     _____                            _
-|  ____|  __ \ / ____|      | |  | |/ ____|  __ \   / ____|                          | |
-| |__  | |__) | |  __ ______| |  | | (___ | |  | | | |     ___  _ __  _ __   ___  ___| |_ ___  _ __
-|  __| |  _  /| | |_ |______| |  | |\___ \| |  | | | |    / _ \| '_ \| '_ \ / _ \/ __| __/ _ \| '__|
-| |____| | \ \| |__| |      | |__| |____) | |__| | | |___| (_) | | | | | | |  __/ (__| || (_) | |
-|______|_|  \_\\_____|       \____/|_____/|_____/   \_____\___/|_| |_|_| |_|\___|\___|\__\___/|_|
-==================================================================================================
-"#;
+// Number of nanoErgs in a single Erg
+static NANO_ERG_CONVERSION: f64 = 1000000000.0;
 
 static CG_RATE_URL: &str =
     "https://api.coingecko.com/api/v3/simple/price?ids=ergo&vs_currencies=USD";
 
 /// Get the Erg/USD price from the nanoErgs per 1 USD datapoint price
 pub fn generate_current_price(datapoint: u64) -> f64 {
-    (1.0 / datapoint as f64) * 1000000000.0
+    (1.0 / datapoint as f64) * NANO_ERG_CONVERSION
 }
 
 /// Acquires the price of Ergs in USD from CoinGecko and convert it
@@ -31,7 +24,8 @@ fn get_nanoerg_usd_price() -> Result<u64> {
     let resp = reqwest::blocking::Client::new().get(CG_RATE_URL).send()?;
     let price_json = json::parse(&resp.text()?)?;
     if let Some(p) = price_json["ergo"]["usd"].as_f64() {
-        let nanoerg_price = (1.0 / p as f64) * 1000000000.0;
+        // Convert from price Erg/USD to nanoErgs per 1 USD
+        let nanoerg_price = (1.0 / p) * NANO_ERG_CONVERSION;
         return Ok(nanoerg_price as u64);
     } else {
         Err(anyhow!("Failed to parse price from json."))
