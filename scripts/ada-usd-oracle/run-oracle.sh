510de11c9f5c9dbeae13bfc7a67808230d2b4b8a
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-08-21 10:12:41 -0400

    Reworked deploy scripts for multiple connectors

---
+++ scripts/ada-usd-oracle/run-oracle.sh
@@ -0,0 +1,5 @@
+screen -S OracleCore -t 0 -A -d -m
+screen -S OracleCore -X screen -t 1
+screen -S OracleCore -p 0 -X stuff './oracle-core\n'
+screen -S OracleCore -p 1 -X stuff './ada-usd-connector\n'
+screen -R OracleCore -p 0
--- scripts/erg-usd-oracle/prepare-erg-usd-oracle.sh
+++ scripts/erg-usd-oracle/prepare-erg-usd-oracle.sh
@@ -1,14 +1,14 @@
 # This script builds the oracle core & the erg-usd connector
 # and then copies the executables + config file into a final
-# `oracle-core-deployed` folder.
+# `erg-usd-oracle-deployed` folder.
 
-mkdir ../../oracle-core-deployed
-cp run-oracle.sh ../../oracle-core-deployed
+mkdir ../../erg-usd-oracle-deployed
+cp run-oracle.sh ../../erg-usd-oracle-deployed
 cd ../..
-cp oracle-config.yaml oracle-core-deployed
+cp oracle-config.yaml erg-usd-oracle-deployed
 cargo build --release
-cp target/release/oracle-core oracle-core-deployed
+cp target/release/oracle-core erg-usd-oracle-deployed
 cd connectors/erg-usd-connector
 cargo build --release
-cp target/release/erg-usd-connector ../../oracle-core-deployed
-cd ../../oracle-core-deployed
+cp target/release/erg-usd-connector ../../erg-usd-oracle-deployed
+cd ../../erg-usd-oracle-deployed
