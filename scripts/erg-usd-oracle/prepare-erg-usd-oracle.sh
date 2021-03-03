82ed8667dc95faeb847a9c74dce5ddf1facad3d5
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2021-03-03 16:22:13 +0100

    Updated prepare script for hardened oracles

--- scripts/erg-usd-oracle/prepare-erg-usd-oracle.sh
+++ scripts/erg-usd-oracle/prepare-erg-usd-oracle.sh
@@ -2,13 +2,13 @@
 # and then copies the executables + config file into a final
 # `erg-usd-oracle-deployed` folder.
 
-mkdir ../../erg-usd-oracle-deployed
-cp run-oracle.sh oracle-config.yaml ../../erg-usd-oracle-deployed
+mkdir ../../hardened-erg-usd-oracle-deployed
+cp run-oracle.sh oracle-config.yaml ../../hardened-erg-usd-oracle-deployed
 cd ../..
-cp oracle-config.yaml erg-usd-oracle-deployed
+cp oracle-config.yaml hardened-erg-usd-oracle-deployed
 cargo build --release
-cp target/release/oracle-core erg-usd-oracle-deployed
+cp target/release/oracle-core hardened-erg-usd-oracle-deployed
 cd connectors/erg-usd-connector
 cargo build --release
-cp target/release/erg-usd-connector ../../erg-usd-oracle-deployed
-cd ../../erg-usd-oracle-deployed
+cp target/release/erg-usd-connector ../../hardened-erg-usd-oracle-deployed
+cd ../../hardened-erg-usd-oracle-deployed
