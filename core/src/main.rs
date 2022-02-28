1b1629cb469675af79de462d45dc719c92cb8501
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2022-02-28 17:52:26 +0200

    add relevant clippy rules from sigma-rust;

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,6 +1,17 @@
+// Coding conventions
 #![allow(dead_code)]
 #![allow(clippy::redundant_clone)]
 #![allow(clippy::ptr_arg)]
+#![allow(clippy::unit_arg)]
+#![forbid(unsafe_code)]
+#![deny(non_upper_case_globals)]
+#![deny(non_camel_case_types)]
+#![deny(non_snake_case)]
+#![deny(unused_mut)]
+#![deny(unused_imports)]
+#![deny(clippy::wildcard_enum_match_arm)]
+#![deny(clippy::todo)]
+#![deny(clippy::unimplemented)]
 
 #[macro_use]
 extern crate json;
