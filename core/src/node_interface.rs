2cc95cdd6c7010bc65185c2e0e27b004d652c0f1
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2022-04-19 18:24:47 +0300

    draft get_wallet_change_address() implementation;

--- core/src/node_interface.rs
+++ core/src/node_interface.rs
@@ -1,7 +1,10 @@
 use crate::oracle_config::{get_node_api_key, get_node_ip, get_node_port};
 use ergo_lib::{
     chain::transaction::unsigned::UnsignedTransaction,
-    ergotree_ir::chain::{address::Address, ergo_box::ErgoBox},
+    ergotree_ir::chain::{
+        address::{Address, AddressEncoder, NetworkPrefix},
+        ergo_box::ErgoBox,
+    },
 };
 use ergo_node_interface::{
     node_interface::{NodeError, NodeInterface},
@@ -121,7 +124,14 @@ pub fn current_block_height() -> Result<BlockHeight> {
 }
 
 pub fn get_wallet_change_address() -> Result<Address> {
-    todo!()
+    let status = new_node_interface().wallet_status()?;
+    let address_encoder = AddressEncoder::new(NetworkPrefix::Mainnet);
+    // TODO: change_address can be empty if wallet is locked or not initialized
+    // TODO: handle address parsing error
+    let address = address_encoder
+        .parse_address_from_str(&status.change_address.unwrap())
+        .unwrap();
+    Ok(address)
 }
 
 /// Sign an `UnsignedTransaction` and then submit it to the mempool.
