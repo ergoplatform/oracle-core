93103349e5f2800338c33e38f406666e98a5d80f
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2022-10-12 17:54:39 +0300

    log and ignore double spending attempt node error for refresh action #139;

--- core/src/actions.rs
+++ core/src/actions.rs
@@ -34,35 +34,32 @@ pub enum ActionExecError {
 }
 
 pub fn execute_action(action: PoolAction) -> Result<(), ActionExecError> {
-    match action {
-        PoolAction::Refresh(action) => {
-            log::debug!("Executing refresh action: {:?}", action);
-            execute_refresh_action(action)
+    let exec_res = match action {
+        PoolAction::Refresh(action) => execute_refresh_action(action),
+        PoolAction::PublishDatapoint(action) => execute_publish_datapoint_action(action),
+    };
+    match exec_res {
+        Ok(_) => Ok(()),
+        Err(ActionExecError::NodeError(NodeError::BadRequest(msg)))
+            if msg.as_str() == "Double spending attempt" =>
+        {
+            log::info!("Node returned double spending attempt error, probably due to our previous tx is still in the mempool)");
+            Ok(())
         }
-        PoolAction::PublishDatapoint(action) => {
-            log::debug!("Executing publish datapoint action: {:?}", action);
-            execute_publish_datapoint_action(action)
-        }
+        Err(e) => Err(e),
     }
 }
 
 fn execute_refresh_action(action: RefreshAction) -> Result<(), ActionExecError> {
+    log::debug!("Executing refresh action: {:?}", action);
     let tx_id = sign_and_submit_transaction(&action.tx)?;
     log::info!("Refresh action executed successfully, tx id: {}", tx_id);
     Ok(())
 }
 
 fn execute_publish_datapoint_action(action: PublishDataPointAction) -> Result<(), ActionExecError> {
-    match sign_and_submit_transaction(&action.tx) {
-        Ok(tx_id) => {
-            log::info!("Datapoint published successfully, tx id: {}", tx_id);
-        }
-        Err(NodeError::BadRequest(msg)) if msg.as_str() == "Double spending attempt" => {
-            log::info!("Failed commiting datapoint (double spending attempt error, probably due to our previous data point tx is still in the mempool)");
-        }
-        Err(e) => {
-            return Err(ActionExecError::NodeError(e));
-        }
-    };
+    log::debug!("Executing publish datapoint action: {:?}", action);
+    let tx_id = sign_and_submit_transaction(&action.tx)?;
+    log::info!("Datapoint published successfully, tx id: {}", tx_id);
     Ok(())
 }
