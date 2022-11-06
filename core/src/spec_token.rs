f207c98f2874283e3def579d8bee8a731373ea6e
Kamal Ahmad <kamalahmad22@pm.me>
2022-11-06 05:02:31 +0500

    Add TryFrom<Token>/Into<Token> for SpecToken

--- core/src/spec_token.rs
+++ core/src/spec_token.rs
@@ -1,22 +1,57 @@
+use std::convert::TryFrom;
+
 use ergo_lib::ergotree_ir::chain::token::TokenAmount;
 /// Special tokens (newtypes) for tokens used in oracle_core
 use ergo_lib::ergotree_ir::chain::token::TokenId;
+use ergo_lib::ergotree_ir::chain::token::Token;
 use serde::Deserialize;
 use serde::Serialize;
 
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct SpecToken<T: TokenIdKind> {
-    id: T,
-    amount: TokenAmount,
+    pub id: T,
+    pub amount: TokenAmount,
 }
 
-impl<T: TokenIdKind> SpecToken<T> {
-    fn token_id(&self) -> TokenId {
-        self.id.token_id()
+impl<T: TokenIdKind> Into<Token> for SpecToken<T> {
+    fn into(self) -> Token {
+        Token {
+            token_id: self.id.token_id(),
+            amount: self.amount
+        }
     }
 }
 
-pub trait TokenIdKind {
+impl<T: TokenIdKind> TryFrom<Token> for SpecToken<T> {
+    type Error = String;
+    fn try_from(token: Token) -> Result<Self, Self::Error> {
+        let token_id = T::load_from_oracle_config()?;
+        if token.token_id != token_id.token_id() {
+            return Err("Token ID does not match ORACLE_CONFIG".into());
+        }
+        Ok(SpecToken {
+            id: token_id,
+            amount: token.amount
+        })
+    }
+
+}
+
+impl<T: TokenIdKind> SpecToken<T> {
+    pub fn token_id(&self) -> TokenId {
+        self.id.token_id()
+    }
+    pub fn token_amount(&self) -> TokenAmount {
+        self.amount
+    }
+}
+
+pub trait TokenIdKind: Sized {
     fn token_id(&self) -> TokenId;
+    /// Create a new TokenIdKind from a TokenId. Note that this does not validate the token id against the config file
+    fn from_token_id_unchecked(token: TokenId) -> Self;
+    /// Create a new TokenIdKind from ORACLE_CONFIG
+    fn load_from_oracle_config() -> Result<Self, String>;
 }
 
 #[derive(Serialize, Deserialize, Eq, PartialEq, Clone, Debug)]
@@ -26,6 +61,12 @@ pub struct PoolTokenId(TokenId);
 impl TokenIdKind for PoolTokenId {
     fn token_id(&self) -> TokenId {
         self.0.clone()
+    }
+    fn from_token_id_unchecked(token: TokenId) -> Self {
+        Self(token)
+    }
+    fn load_from_oracle_config() -> Result<Self, String> {
+        crate::oracle_config::MAYBE_ORACLE_CONFIG.map(|cfg| cfg.token_ids.pool_nft_token_id)
     }
 }
 
@@ -37,6 +78,12 @@ impl TokenIdKind for UpdateTokenId {
     fn token_id(&self) -> TokenId {
         self.0.clone()
     }
+    fn from_token_id_unchecked(token: TokenId) -> Self {
+        Self(token)
+    }
+    fn load_from_oracle_config() -> Result<Self, String> {
+        crate::oracle_config::MAYBE_ORACLE_CONFIG.map(|cfg| cfg.token_ids.update_nft_token_id)
+    }
 }
 
 #[derive(Serialize, Deserialize, Eq, PartialEq, Clone, Debug)]
@@ -47,6 +94,12 @@ impl TokenIdKind for RefreshTokenId {
     fn token_id(&self) -> TokenId {
         self.0.clone()
     }
+    fn from_token_id_unchecked(token: TokenId) -> Self {
+        Self(token)
+    }
+    fn load_from_oracle_config() -> Result<Self, String> {
+        crate::oracle_config::MAYBE_ORACLE_CONFIG.map(|cfg| cfg.token_ids.refresh_nft_token_id)
+    }
 }
 
 #[derive(Serialize, Deserialize, Eq, PartialEq, Clone, Debug)]
@@ -55,6 +108,12 @@ pub struct RewardTokenId(TokenId);
 impl TokenIdKind for RewardTokenId {
     fn token_id(&self) -> TokenId {
         self.0.clone()
+    }
+    fn from_token_id_unchecked(token: TokenId) -> Self {
+        Self(token)
+    }
+    fn load_from_oracle_config() -> Result<Self, String> {
+        crate::oracle_config::MAYBE_ORACLE_CONFIG.map(|cfg| cfg.token_ids.reward_token_id)
     }
 }
 
@@ -66,6 +125,12 @@ impl TokenIdKind for OracleTokenId {
     fn token_id(&self) -> TokenId {
         self.0.clone()
     }
+    fn from_token_id_unchecked(token: TokenId) -> Self {
+        Self(token)
+    }
+    fn load_from_oracle_config() -> Result<Self, String> {
+        crate::oracle_config::MAYBE_ORACLE_CONFIG.map(|cfg| cfg.token_ids.oracle_token_id)
+    }
 }
 
 #[derive(Serialize, Deserialize, Eq, PartialEq, Clone, Debug)]
@@ -74,5 +139,11 @@ pub struct BallotTokenId(TokenId);
 impl TokenIdKind for BallotTokenId {
     fn token_id(&self) -> TokenId {
         self.0.clone()
+    }
+    fn from_token_id_unchecked(token: TokenId) -> Self {
+        Self(token)
+    }
+    fn load_from_oracle_config() -> Result<Self, String> {
+        crate::oracle_config::MAYBE_ORACLE_CONFIG.map(|cfg| cfg.token_ids.ballot_token_id)
     }
 }
